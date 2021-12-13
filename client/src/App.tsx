79f369c33effc0d95813c23e7d31abbe7c093758
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-12-13 15:16:48 -0800

    added css loader to allow import of css files :)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import './App.css';
 import Header from './components/Header'
 
 const App = () => {
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,17 @@ const path = require('path');
 
 module.exports = {
     entry: './client/src/index.tsx',
-    mode:'development',
+    mode: 'development',
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
             }
         ]
     },
